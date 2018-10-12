a5b8c46a2d1e40d4c7168cffaddb3931e06ef8e0
raymond <raymond.the.dev@gmail.com>
2018-10-12 17:02:50 +0800

    git init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raymond
+ * Date: 12/10/2018
+ * Time: 5:01 PM
+ */
+
+function think($has_idea){
+    if($has_idea = 1){
+        return true;
+    }
+    return false;
+}
+
+think(0);
